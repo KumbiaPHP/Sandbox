028211c2f7d92dd463ea2cc04a0b6397028fdfcc
Joanhey <joanhey@kumbiaphp.com>
2020-04-06 23:43:04 +0200

    Add sociable example For a lot of partials we can use pages

---
+++ app/controllers/partials/social_controller.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Controller por defecto si no se usa el routes
+ *
+ */
+class SocialController extends AppController
+{
+
+    public function index()
+    {
+
+    }
+
+    public function sociable()
+    {
+
+    }
+}
