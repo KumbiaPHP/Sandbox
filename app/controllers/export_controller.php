1a6978e6952bf738dbd8c664d8b2e145d7248d4f
Henry Stivens Adarme M <henry.stivens@gmail.com>
2020-04-12 17:34:49 -0500

    New controller to export data in several formats

---
+++ app/controllers/export_controller.php
@@ -0,0 +1,18 @@
+<?php
+
+class ExportController extends AppController
+{    
+
+    public function pdf()
+    {
+        View::response('pdf');
+        //Usa el tempalte 'pdf'
+        View::select('index', 'pdf');
+        //Modifica el nombre del archivo a descargar
+        $this->fileName = 'user-list';
+        //Modifica el título del documento PDF en la cabecera
+        $this->title = 'User List';
+        $this->data = User::all();
+        $this->download = false;
+    }
+}
