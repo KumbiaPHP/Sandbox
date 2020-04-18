5f11996c041400e111fa752200dfddf5d2fffec5
Henry Stivens Adarme M <henry.stivens@gmail.com>
2020-04-18 07:23:40 -0500

    Added new controller and views to the PDF example

---
+++ app/controllers/templates_examples_controller.php
@@ -0,0 +1,22 @@
+<?php
+
+class TemplatesExamplesController extends AppController
+{
+
+    public function info($view)
+    {
+        View::response($view);
+    }
+
+    public function pdf()
+    {
+        //Usa el tempalte 'pdf'
+        View::template('pdf');
+        //Modifica el nombre del archivo a descargar
+        $this->fileName = 'user-list';
+        //Modifica el título del documento PDF en la cabecera
+        $this->title = 'User List';
+        $this->data = User::all();
+        $this->download = false;
+    }
+}
