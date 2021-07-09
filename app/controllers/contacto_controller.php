61e0a250c335a1c4ebfa65c7d2df2d683d7548f4
Joanhey <joanhey@kumbiaphp.com>
2021-07-09 11:30:52 +0200

    Format code

--- app/controllers/contacto_controller.php
+++ app/controllers/contacto_controller.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Controller para el envio de un formulario de contacto básico
  */
@@ -8,23 +9,22 @@ class ContactoController extends AppController
     {
         // Si es AJAX enviar solo el view
         if (Input::isAjax()) {
-          View::template(null);
+            View::template(null);
         }
-
     }
 
-    public function contacto() {
+    public function contacto()
+    {
         if (Input::hasPost('contacto')) {
             $aDatos = Input::post('contacto');
             $respuesta = (new Contacto)->sendEmail($aDatos);
-            
+
             if ($respuesta) {
                 Flash::valid('Mensaje enviado correctamente');
                 return;
             }
 
             Flash::error('Mensaje NO enviado. Vuelva a probarlo más tarde');
-
         }
     }
 }
--- app/models/contacto.php
+++ app/models/contacto.php
@@ -2,7 +2,8 @@
 class Contacto
 {
 
-    public function sendEmail($aDatos) {
+    public function sendEmail($aDatos)
+    {
 
         $config = Config::get('mail.headers');
 
@@ -14,15 +15,15 @@ class Contacto
 
         // Definimos la cabecera
         $cabecera  = 'MIME-Version: 1.0' . "\r\n";
-        $cabecera .= 'Content-type: text/html; charset='. $config["charset"] . "\r\n";
-        $cabecera .= "From: ". $config["from_name"] ." <" .$config["from_email"]. ">\r\n";
+        $cabecera .= 'Content-type: text/html; charset=' . $config["charset"] . "\r\n";
+        $cabecera .= "From: " . $config["from_name"] . " <" . $config["from_email"] . ">\r\n";
 
         // Definimos el cuerpo del mensaje
-        $mensaje  = "<br>Nombre: ". $aDatos["nombre"];
-        $mensaje .= "<br>Correo : ".$aDatos["email"];
-        $mensaje .= "<br>Mensaje : ".nl2br($aDatos["mensaje"]);
+        $mensaje  = "<br>Nombre: " . $aDatos["nombre"];
+        $mensaje .= "<br>Correo : " . $aDatos["email"];
+        $mensaje .= "<br>Mensaje : " . nl2br($aDatos["mensaje"]);
 
         // Enviamos el correo
-        return mail($correo,$asunto,$mensaje,$cabecera);
+        return mail($correo, $asunto, $mensaje, $cabecera);
     }
 }
